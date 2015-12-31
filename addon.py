9a9c480da67e50dcade9572aff62befa352f8251
Adrian L. Shaw <adrianlshaw@gmail.com>
2015-12-31 00:53:31 +0000

    New improvements include sanitising filepaths for whitespace and detecting remote shares. Dialog box is shown when files are not local.

--- addon.py
+++ addon.py
@@ -1,12 +1,21 @@
 import sys
-import xbmc
 import time
-
+import xbmc
+import xbmcgui
+import xbmcaddon
 # Grab file name and add it to the queue
 if __name__ == '__main__':
-	filename = sys.listitem.getInfoLabel("ListItem.FileNameAndPath")
-	message = "Clicked on '%s'" % sys.listitem.getLabel()
-	xbmc.executebuiltin("Notification(\"%s\", \"Added to DVD queue\")" % message)
-	f = open("/tmp/dvd_queue.txt","a")
-	f.write(filename)
-	f.close()
+	filename = xbmc.getInfoLabel('ListItem.FileNameAndPath')
+	filename = filename.replace(" ", "\ ")
+	if filename.startswith('smb://'):
+		__addon__ = xbmcaddon.Addon()
+		__addonname__ = __addon__.getAddonInfo('name')
+		title = "DVD burner"
+		xbmcgui.Dialog().ok(__addonname__, "Sorry, this file cannot be burned.",
+			"Network shares like SMB and NFS are unsupported.", "")
+	else:
+		f = open("/tmp/dvd_queue.txt","a")
+		f.write(filename + "\n")
+		f.close()
+		time.sleep(3)
+		xbmc.executebuiltin("Notification(\"%s\", \"Added to DVD queue\")" % filename)
